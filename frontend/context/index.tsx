de02ec1296b39beff532d33c0a4a4426025645ce
Dalvin <66400608+d41vin@users.noreply.github.com>
2025-10-04 23:59:21 +0800

    feat(appkit): add Reown AppKit setup with wagmi integration

---
+++ frontend/context/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { wagmiAdapter, projectId } from "@/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import { hederaTestnet } from "@reown/appkit/networks";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+
+// Set up queryClient
+const queryClient = new QueryClient();
+
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+// Set up metadata
+const metadata = {
+  name: "Pact",
+  description:
+    "An all-in-one finance app for personal, group, and merchant transactions on Hedera, with programmable features like bill splitting, group-based P2P lending, borrowing, betting, and more.",
+  url: "https://appkitexampleapp.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [hederaTestnet],
+  defaultNetwork: hederaTestnet,
+  metadata: metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+function ContextProvider({
+  children,
+  cookies,
+}: {
+  children: ReactNode;
+  cookies: string | null;
+}) {
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies,
+  );
+
+  return (
+    <WagmiProvider
+      config={wagmiAdapter.wagmiConfig as Config}
+      initialState={initialState}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default ContextProvider;
