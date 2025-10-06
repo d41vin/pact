a6d8c0607268165e14219a8a96de812c7a86761d
Dalvin <66400608+d41vin@users.noreply.github.com>
2025-10-06 23:59:32 +0800

    feat(appkit): enable SIWX and configure AppKit theming

--- frontend/context/index.tsx
+++ frontend/context/index.tsx
@@ -6,6 +6,7 @@ import { createAppKit } from "@reown/appkit/react";
 import { hederaTestnet } from "@reown/appkit/networks";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+import { ReownAuthentication } from "@reown/appkit-siwx";
 
 // Set up queryClient
 const queryClient = new QueryClient();
@@ -30,8 +31,29 @@ const modal = createAppKit({
   networks: [hederaTestnet],
   defaultNetwork: hederaTestnet,
   metadata: metadata,
+  siwx: new ReownAuthentication(),
   features: {
     analytics: true, // Optional - defaults to your Cloud configuration
+    connectMethodsOrder: ["email", "social", "wallet"],
+    email: true, // default to true
+    socials: ["google", "apple"],
+    emailShowWallets: false, // default to true
+  },
+  featuredWalletIds: [
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    "a29498d225fa4b13468ff4d6cf4ae0ea4adcbd95f07ce8a843a1dee10b632f3f",
+  ],
+  debug: true, //defaults to false - remove before production
+  themeMode: "light", //defaults to user system settings
+  themeVariables: {
+    "--w3m-font-family": "poppins, sans-serif",
+    "--w3m-accent": "#0898f9",
+    // "--w3m-color-mix": "red",
+    // "--w3m-color-mix-strength": 3,
+    // "--w3m-qr-color": "green",
+    //'--w3m-font-size-master': "string",
+    // "--w3m-border-radius-master": "string",
+    // "--apkt-tokens-core-backgroundAccentPrimary-base": "#0898f9",
   },
 });
 
