43394df5410a03cb801e32878154d840e4d5f35a
Dalvin <66400608+d41vin@users.noreply.github.com>
2025-12-05 21:00:18 +0800

    chore(config): add serverExternalPackages and webpack null-loader rule to fix build issues

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,28 @@ import type { NextConfig } from "next";
 import * as path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {
     root: path.join(__dirname, "../frontend"),
+  },
+
+  serverExternalPackages: [
+    "pino",
+    "thread-stream",
+    "@walletconnect/logger",
+    "@walletconnect/utils",
+    "@walletconnect/universal-provider",
+    "@reown/appkit",
+    "@reown/appkit-controllers",
+    "@reown/appkit-adapter-wagmi",
+  ],
+
+  webpack(config) {
+    config.module?.rules?.push({
+      test: /node_modules[\\/](thread-stream|pino)[\\/].*test.*\.js$/,
+      use: "null-loader",
+    });
+
+    return config;
   },
 };
 
